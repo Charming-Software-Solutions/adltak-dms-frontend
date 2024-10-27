108f70ac694f196549c3ccccb8a9add2e82d09ea
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-10-27 08:43:25 +0800

    refactor(users): updated auth

--- app/(root)/users/page.tsx
+++ app/(root)/users/page.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import UserClient from "./UserClient";
-import { getSession } from "@/lib/auth";
-import { getUsers } from "@/lib/actions/user.actions";
+import { getUserById, getUsers } from "@/lib/actions/user.actions";
+import { getSession } from "@/lib/session";
+import { redirect } from "next/navigation";
 
 export default async function UsersPage() {
+  const session = await getSession();
   const users = await getUsers();
 
-  return <UserClient user={(await getSession()).user} users={users} />;
+  if (!session) {
+    redirect("/login");
+  }
+  const user = await getUserById(session.userId, session.access);
+
+  if (!user.data) {
+    throw Error("User not found");
+  }
+
+  return <UserClient user={user.data} users={users} />;
 }
