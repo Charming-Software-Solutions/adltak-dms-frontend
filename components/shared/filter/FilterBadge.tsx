727db4bfcf7c71f5ae3ebc2278399d35a28c87d4
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-11-10 17:45:42 +0800

    feat(FilterBadge): added red border color and background

--- components/shared/filter/FilterBadge.tsx
+++ components/shared/filter/FilterBadge.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const FilterBadge = ({ label, value, onRemove }: Props) => {
   return (
-    <Card className="relative w-auto py-2 px-3.5 text-sm cursor-pointer max-w-xs truncate h-10">
+    <Card className="relative w-auto py-2 px-3.5 text-sm cursor-pointer max-w-xs truncate h-10 border-red-200 bg-red-100">
       <div className="flex items-center justify-center">
         <span>
           {label}:<strong className="ml-1 font-semibold">{value}</strong>
