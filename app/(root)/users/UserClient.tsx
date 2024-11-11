9e805185c6998bad412b4f57c959ad1c18ffafcf
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-11-11 10:53:05 +0800

    refactor: hooks cleanup

--- app/(root)/users/UserClient.tsx
+++ app/(root)/users/UserClient.tsx
@@ -1,25 +1,13 @@
 "use client";
 
-import Header from "@/components/shared/Header";
-import { Button } from "@/components/ui/button";
-import { useUserForm } from "@/hooks";
-import { User } from "@/types/user";
-import { PlusCircle } from "lucide-react";
-import React, { useEffect, useState } from "react";
-import UserForm from "./componets/UserForm";
-import { DrawerClose } from "@/components/ui/drawer";
-import { z } from "zod";
-import { userFormSchema } from "@/schemas";
-import { ICreateUser } from "@/interfaces";
-import { getUserRoleBooleans } from "@/lib/utils";
-import { createUser } from "@/lib/actions/user.actions";
-import { ApiResponse } from "@/types/api";
-import { useRouter } from "next/navigation";
-import { DataTable } from "@/components/shared/table/data-table";
 import {
   UserColumns,
   visibileUserColumns,
 } from "@/components/shared/table/columns/UserColumns";
+import { DataTable } from "@/components/shared/table/data-table";
+import { User } from "@/types/user";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 import { useMediaQuery } from "react-responsive";
 
 type Props = {
@@ -31,31 +19,11 @@ const UserClient = ({ user, users }: Props) => {
   const [isMounted, setIsMounted] = useState(false);
   const [openDialog, setOpenDialog] = useState(false);
   const isDesktop = useMediaQuery({ query: "(min-width: 1224px)" });
-  const userForm = useUserForm();
   const router = useRouter();
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
-
-  const onSubmit = async (values: z.infer<typeof userFormSchema>) => {
-    const userRole = getUserRoleBooleans(values.role);
-
-    const user: ICreateUser = {
-      email: values.email,
-      password: values.password,
-      ...userRole,
-    };
-
-    const result: ApiResponse<User> = await createUser(user);
-
-    if (result.errors) {
-      console.log(result.errors);
-    } else {
-      router.refresh();
-      userForm.reset();
-    }
-  };
 
   return (
     <React.Fragment>
