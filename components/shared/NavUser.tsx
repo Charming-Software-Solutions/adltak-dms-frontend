ccca14ec5409f51069520461025d43aa3491f48b
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-11-19 16:17:22 +0800

    refactor(components): revamped sidebar footer

---
+++ components/shared/NavUser.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { BadgeCheck, ChevronsUpDown, LogOut } from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+import { formatUserRole } from "@/lib/utils";
+import { EmployeeLogin, User } from "@/types/user";
+import NavUserProfile from "./NavUserProfile";
+import { logout } from "@/lib/actions/auth.actions";
+
+export function NavUser({
+  user,
+  refresh,
+  employee,
+}: {
+  user: User;
+  refresh: string;
+  employee?: EmployeeLogin;
+}) {
+  const { isMobile } = useSidebar();
+  const avatarImage = employee?.profile_image || "/assets/images/avatar.jpg";
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground rounded-lg bg-card border text-card-foreground shadow-sm py-8"
+            >
+              <NavUserProfile
+                title={employee?.name ?? "Admin"}
+                subtitle={formatUserRole(user.role)}
+                alt={"profile-image"}
+                avatarImage={avatarImage}
+              />
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <NavUserProfile
+                title={employee?.name ?? "Admin"}
+                subtitle={formatUserRole(user.role)}
+                alt={"profile-image"}
+                avatarImage={avatarImage}
+              />
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <BadgeCheck className="size-4 mr-2" />
+                Account
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={() => {
+                logout(refresh);
+              }}
+            >
+              <LogOut className="size-4 mr-2" />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
