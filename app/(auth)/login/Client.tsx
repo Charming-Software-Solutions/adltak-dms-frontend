702cf5e06b845420852b489b51b43ff3ec1447f7
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-11-25 18:44:11 +0800

    refactor: revamped login form
    - added a proper form
    - added error handling and a loading indication

--- app/(auth)/login/Client.tsx
+++ app/(auth)/login/Client.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import DialogFormButton from "@/components/shared/buttons/DialogFormButton";
+import CustomFormField, {
+  FormFieldType,
+  InputType,
+} from "@/components/shared/CustomFormField";
 import {
   Card,
   CardContent,
@@ -9,60 +13,105 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
 import { Label } from "@/components/ui/label";
 import { login } from "@/lib/actions/auth.actions";
+import { loginSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useFormStatus } from "react-dom";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
 
 const LoginClient = () => {
-  const { pending } = useFormStatus();
   const router = useRouter();
 
-  const handleSubmit = async (formData: FormData) => {
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
+  const handleSubmit = async (values: z.infer<typeof loginSchema>) => {
     try {
-      await login(email, password);
+      await login(values.email, values.password);
       router.push("/");
     } catch (error) {
-      console.error("Login failed:", error);
+      if (error instanceof Error) {
+        try {
+          const errorDetails = JSON.parse(error.message);
+          const errorMessage =
+            errorDetails?.detail || "An unexpected error occurred";
+          toast.error(errorMessage, {
+            position: "top-center",
+          });
+        } catch (parseError) {
+          toast.error("An error occurred. Please try again.", {
+            position: "top-center",
+          });
+        }
+      } else {
+        toast.error("An unknown error occurred", {
+          position: "top-center",
+        });
+      }
     }
   };
 
   return (
-    <form action={handleSubmit}>
+    <Form {...form}>
       <Card className="w-full max-w-sm">
         <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
+          <div className="flex items-center justify-between">
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <Image
+              alt="Product image"
+              priority
+              className="aspect-square border rounded-md object-cover"
+              height={35}
+              width={35}
+              src={"/assets/images/logo.jpg"}
+            />
+          </div>
           <CardDescription>
             Enter your email below to login to your account.
           </CardDescription>
         </CardHeader>
         <CardContent className="grid gap-4">
           <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
+            <CustomFormField
+              fieldType={FormFieldType.INPUT}
+              inputType={InputType.EMAIL}
+              control={form.control}
               name="email"
-              placeholder="m@example.com"
-              required
+              label="Email"
+              placeholder={"johndoe@email.com"}
+              disabled={form.formState.isSubmitting}
             />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" name="password" required />
+            <CustomFormField
+              fieldType={FormFieldType.PASSWORD}
+              control={form.control}
+              name="password"
+              label="Password"
+              placeholder="*************"
+              disabled={form.formState.isSubmitting}
+            />
           </div>
         </CardContent>
         <CardFooter>
-          <Button type="submit" disabled={pending} className="w-full">
-            Sign in
-          </Button>
+          <DialogFormButton
+            text="Sign In"
+            disabled={form.formState.isSubmitting}
+            loading={form.formState.isSubmitting}
+            onClick={() => form.handleSubmit(handleSubmit)()}
+          />
         </CardFooter>
       </Card>
-    </form>
+    </Form>
   );
 };
 
