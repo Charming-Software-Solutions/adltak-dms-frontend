24684eec01e578a8ac3629a058ed7d535e940412
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2024-12-10 18:36:35 +0800

    feat(products): added back the expiration filter but for products in allocations

--- app/(root)/products/page.tsx
+++ app/(root)/products/page.tsx
@@ -6,18 +6,22 @@ import {
 } from "@/lib/actions/product.classications.actions";
 import ProductClient from "./ProductClient";
 import { getSession } from "@/lib/session";
+import { getDistributionProducts } from "@/lib/actions/distribution.actions";
 
 export default async function Products() {
   const products = await getProducts();
+  const allocationProducts = await getDistributionProducts();
   const brands = await getBrands();
   const categories = await getCategories();
   const types = await getTypes();
   const user = (await getSession())!.user;
+  console.log(allocationProducts);
 
   return (
     <ProductClient
       user={user}
       products={products}
+      allocationProducts={allocationProducts}
       brands={brands}
       categories={categories}
       types={types}
