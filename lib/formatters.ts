fce7adbfe408b4547c3f3e3b99b1a7ac85ec36cb
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2025-02-28 04:14:13 +0800

    refactor(formatters): made formatErrorResponse accept arrays too

--- lib/formatters.ts
+++ lib/formatters.ts
@@ -3,6 +3,12 @@ import { capitalize } from "./utils";
 
 export const formatErrorResponse = (errors: ErrorResponse): string => {
   return Object.entries(errors)
-    .map(([field, messages]) => `${capitalize(field)}: ${messages.join(", ")}`)
+    .map(([field, messages]) => {
+      // Check if messages is an array, if not, convert it to an array
+      const messageStr = Array.isArray(messages)
+        ? messages.join(", ")
+        : messages;
+      return `${capitalize(field)}: ${messageStr}`;
+    })
     .join("\n");
 };
