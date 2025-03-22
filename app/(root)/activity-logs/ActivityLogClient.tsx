e3fa2212e30103c985793e03cc879216d1a9f192
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2025-03-22 18:16:35 +0800

    refactor(activity-logs): combined filter sheet and body filters into one

--- app/(root)/activity-logs/ActivityLogClient.tsx
+++ app/(root)/activity-logs/ActivityLogClient.tsx
@@ -6,20 +6,10 @@ import {
   visibileActivityLogColumns,
 } from "@/components/shared/table/columns/ActivityLogColumns";
 import { Button } from "@/components/ui/button";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
 import { useResponsive } from "@/hooks";
 import { useDataTable } from "@/hooks/use-datatable";
 import { ActivityLog } from "@/types/activityLog";
-import { FileIcon, ListFilter } from "lucide-react";
+import { FileIcon } from "lucide-react";
 import { parseAsString, useQueryStates } from "nuqs";
 import React from "react";
 import { ActivityLogFilter } from "./components/ActivityLogFilter";
@@ -55,44 +45,7 @@ const ActivityLogsClient = ({ activityLogs }: Props) => {
     },
     filters: (
       <div className="flex ml-2 gap-2">
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="default" className="gap-1">
-              <ListFilter className="h-3.5 w-3.5" />
-              <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                Filter
-              </span>
-            </Button>
-          </SheetTrigger>
-
-          <SheetContent className="flex flex-col h-full">
-            <SheetHeader>
-              <SheetTitle>Activity Logs Filter</SheetTitle>
-              <SheetDescription>
-                Quickly refine activity logs by role, type, and module to
-                pinpoint key entries.
-              </SheetDescription>
-            </SheetHeader>
-
-            <div className="flex-1 overflow-y-auto">
-              <ActivityLogFilter filters={filters} setFilters={setFilters} />
-            </div>
-            <SheetFooter className="pt-2">
-              <div className="flex w-full items-center space-x-2">
-                <SheetClose asChild>
-                  <Button className="w-full flex-grow">View All</Button>
-                </SheetClose>
-                <Button
-                  variant={"outline"}
-                  className="w-full flex-grow"
-                  onClick={() => setFilters(null)}
-                >
-                  Clear All
-                </Button>
-              </div>
-            </SheetFooter>
-          </SheetContent>
-        </Sheet>
+        <ActivityLogFilter filters={filters} setFilters={setFilters} />
       </div>
     ),
   });
--- app/(root)/activity-logs/components/ActivityLogFilter.tsx
+++ app/(root)/activity-logs/components/ActivityLogFilter.tsx
@@ -3,7 +3,18 @@
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { RadioGroupFilter } from "@/types/primitives";
+import { ListFilter } from "lucide-react";
 
 type ActivityLogFilter = {
   role: string;
@@ -95,52 +106,93 @@ export const ActivityLogFilter = ({
   };
 
   return (
-    <div className="pt-6 space-y-10">
-      {radioFilters.group.map((radio, key) => {
-        const fieldName = radio.title.toLowerCase() as keyof ActivityLogFilter;
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline" size="default" className="gap-1">
+          <ListFilter className="h-3.5 w-3.5" />
+          <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+            Filter
+          </span>
+        </Button>
+      </SheetTrigger>
 
-        return (
-          <div className="flex flex-col space-y-4" key={key}>
-            <div className="flex items-center justify-between border-b pb-2">
-              <span className="font-bold">{radio.title}</span>
-              <Button
-                variant={"link"}
-                className="text-sm h-4 text-red-700 pr-0"
-                onClick={() => {
-                  setFilters({ ...filters, [fieldName]: "" });
-                }}
-              >
-                Clear Filter
-              </Button>
-            </div>
-            <RadioGroup
-              className="space-y-4"
-              value={filters[fieldName]}
-              onValueChange={(value) => {
-                setFilters({
-                  ...filters,
-                  [fieldName]: value,
-                });
-              }}
-            >
-              {radio.items.map((item, index) => (
-                <div className="flex items-center justify-between" key={index}>
-                  <Label
-                    htmlFor={String(item.value)}
-                    className="cursor-pointer"
+      <SheetContent className="flex flex-col">
+        <SheetHeader>
+          <SheetTitle>Activity Logs Filter</SheetTitle>
+          <SheetDescription>
+            Quickly refine activity logs by role, type, and module to pinpoint
+            key entries.
+          </SheetDescription>
+        </SheetHeader>
+
+        <div className="overflow-y-auto max-h-[45rem] md:max-h-none md:flex-1 ml-2">
+          <div className="pt-6 space-y-10">
+            {radioFilters.group.map((radio, key) => {
+              const fieldName =
+                radio.title.toLowerCase() as keyof ActivityLogFilter;
+
+              return (
+                <div className="flex flex-col space-y-4" key={key}>
+                  <div className="flex items-center justify-between border-b pb-2">
+                    <span className="font-bold">{radio.title}</span>
+                    <Button
+                      variant={"link"}
+                      className="text-sm h-4 text-red-700 pr-0"
+                      onClick={() => {
+                        setFilters({ ...filters, [fieldName]: "" });
+                      }}
+                    >
+                      Clear Filter
+                    </Button>
+                  </div>
+                  <RadioGroup
+                    className="space-y-4"
+                    value={filters[fieldName]}
+                    onValueChange={(value) => {
+                      setFilters({
+                        ...filters,
+                        [fieldName]: value,
+                      });
+                    }}
                   >
-                    {item.label}
-                  </Label>
-                  <RadioGroupItem
-                    value={String(item.value)}
-                    id={String(item.value)}
-                  />
+                    {radio.items.map((item, index) => (
+                      <div
+                        className="flex items-center justify-between"
+                        key={index}
+                      >
+                        <Label
+                          htmlFor={String(item.value)}
+                          className="cursor-pointer"
+                        >
+                          {item.label}
+                        </Label>
+                        <RadioGroupItem
+                          value={String(item.value)}
+                          id={String(item.value)}
+                        />
+                      </div>
+                    ))}
+                  </RadioGroup>
                 </div>
-              ))}
-            </RadioGroup>
+              );
+            })}
           </div>
-        );
-      })}
-    </div>
+        </div>
+        <SheetFooter className="pt-2">
+          <div className="flex w-full items-center space-x-2">
+            <SheetClose asChild>
+              <Button className="w-full flex-grow">View All</Button>
+            </SheetClose>
+            <Button
+              variant={"outline"}
+              className="w-full flex-grow"
+              onClick={() => setFilters({ role: "", type: "", module: "" })}
+            >
+              Clear All
+            </Button>
+          </div>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   );
 };
