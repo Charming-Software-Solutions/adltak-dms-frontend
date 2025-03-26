d7e7009ff8a9d6b6b34ae8e68f3a613f376938db
Lemuel Guevara <lemueljohn.guevara@gmail.com>
2025-03-26 10:55:55 +0800

    refactor(store): removed persist

--- lib/store.ts
+++ lib/store.ts
@@ -1,6 +1,5 @@
 import { DistributionAsset, DistributionProduct } from "@/types/distribution";
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
 export type AllocationItem = DistributionProduct | DistributionAsset;
 
@@ -18,58 +17,51 @@ type AllocationItemStoreActions = {
 export type AllocationItemStore = AllocationItemStoreState &
   AllocationItemStoreActions;
 
-export const useAllocationStore = create<AllocationItemStore>()(
-  persist(
-    (set) => ({
-      items: [],
+export const useAllocationStore = create<AllocationItemStore>()((set) => ({
+  items: [],
 
-      addItem: (item: AllocationItem) =>
-        set((state) => {
-          // Check if the item already exists
-          const existingItemIndex = state.items.findIndex((i) => i.id === item.id);
+  addItem: (item: AllocationItem) =>
+    set((state) => {
+      // Check if the item already exists
+      const existingItemIndex = state.items.findIndex((i) => i.id === item.id);
+      console.log(item);
 
-          // If it exists, update the quantity
-          if (existingItemIndex >= 0) {
-            const updatedItems = [...state.items];
-            const existingItem = updatedItems[existingItemIndex];
-            updatedItems[existingItemIndex] = {
-              ...existingItem,
-              quantity: (existingItem.quantity || 0) + (item.quantity || 1),
-            };
-            return { items: updatedItems };
-          }
+      // If it exists, update the quantity
+      if (existingItemIndex >= 0) {
+        const updatedItems = [...state.items];
+        const existingItem = updatedItems[existingItemIndex];
+        updatedItems[existingItemIndex] = {
+          ...existingItem,
+          quantity: (existingItem.quantity || 0) + (item.quantity || 1),
+        };
+        return { items: updatedItems };
+      }
 
-          return { items: [...state.items, item] };
-        }),
-
-      removeItem: (itemId: string) =>
-        set((state) => ({
-          items: state.items.filter((item) => item.id !== itemId),
-        })),
-
-      clearItems: (type?: "product" | "asset") => 
-        set((state) => {
-          if (!type) {
-            return { items: [] };
-          }
-          
-          return { 
-            items: state.items.filter(item => 
-              type === "product" ? !("product" in item) : !("asset" in item)
-            ) 
-          };
-        }),
-
-      updateQuantity: (itemId: string, quantity: number) =>
-        set((state) => ({
-          items: state.items.map((item) =>
-            item.id === itemId ? { ...item, quantity } : item,
-          ),
-        })),
+      return { items: [...state.items, item] };
     }),
-    {
-      name: "allocation-storage",
-      partialize: (state) => ({ items: state.items }),
-    },
-  ),
-);
+
+  removeItem: (itemId: string) =>
+    set((state) => ({
+      items: state.items.filter((item) => item.id !== itemId),
+    })),
+
+  clearItems: (type?: "product" | "asset") =>
+    set((state) => {
+      if (!type) {
+        return { items: [] };
+      }
+
+      return {
+        items: state.items.filter((item) =>
+          type === "product" ? !("product" in item) : !("asset" in item),
+        ),
+      };
+    }),
+
+  updateQuantity: (itemId: string, quantity: number) =>
+    set((state) => ({
+      items: state.items.map((item) =>
+        item.id === itemId ? { ...item, quantity } : item,
+      ),
+    })),
+}));
